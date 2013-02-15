bfb5c26fa9f16707c7bd48718517d72b5388e4f1
root <root@puppypc9267.(none)>
2013-02-15 13:17:11 +0000

    kommentar

--- save.sh
+++ save.sh
@@ -17,3 +17,22 @@ case "$jn" in
        * )  echo "Var snäll och svara ja eller nej.";;
 esac
 
+
+saveToGithub() {
+	echo "Ge en kommentar till dina ändringar!";
+	read kommentar;
+        git commit -m kommentar;
+        git push;
+}
+
+echo "Försöker spara alla filer."
+git add -A;
+git status;
+read -p "Vad statusen bra?(j/n)" jn;
+
+case "$jn" in
+   [Jj]* )  saveToGithub;;
+   [Nn]* )  exit;;
+       * )  echo "Var snäll och svara ja eller nej.";;
+esac
+
--- update.sh
+++ update.sh
@@ -3,3 +3,8 @@
      git fetch upstream;
      git merge upstream/master;     
 
+ 
+     echo "Försöker uppdatera alla gemensamma filer."
+     git fetch upstream;
+     git merge upstream/master;     
+
