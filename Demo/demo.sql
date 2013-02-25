597884e478774676f8afa37e65482e29168e83db
unknown <TSKRM@C110-ISJ.skolan.hassleholm.se>
2013-02-25 07:37:50 +0100

    kommentar

--- Demo/demo.sql
+++ Demo/demo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.4.9
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Skapad: 11 feb 2013 kl 10:49
--- Serverversion: 5.5.27-log
--- PHP-version: 5.4.6
+-- Skapad: 25 feb 2013 kl 07:34
+-- Serverversion: 5.5.20
+-- PHP-version: 5.3.9
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,6 +23,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellstruktur `elever`
+--
+
+CREATE TABLE IF NOT EXISTS `elever` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `klass` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumpning av Data i tabell `elever`
+--
+
+INSERT INTO `elever` (`id`, `klass`) VALUES
+(1, '1a'),
+(2, '1a'),
+(3, '1b'),
+(4, '1c');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellstruktur `larare`
 --
 
@@ -30,7 +52,15 @@ CREATE TABLE IF NOT EXISTS `larare` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `legitimation` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumpning av Data i tabell `larare`
+--
+
+INSERT INTO `larare` (`id`, `legitimation`) VALUES
+(1, 'Matematik/Data'),
+(2, 'Svenska/Utlänska');
 
 -- --------------------------------------------------------
 
@@ -46,7 +76,14 @@ CREATE TABLE IF NOT EXISTS `personer` (
   `alder` tinyint(4) NOT NULL,
   `adress` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumpning av Data i tabell `personer`
+--
+
+INSERT INTO `personer` (`id`, `arLara`, `altId`, `namn`, `alder`, `adress`) VALUES
+(1, 1, 1, 'Ada Lovelace', 127, 'Stobygatan 7\r\n281 39  Hässleholm');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
