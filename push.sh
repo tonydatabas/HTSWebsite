8c5cfbcbe3699954e26aafd12ecbb7695dcc4296
Magnus Kronnäs <magnus.kronnas@gmail.com>
2013-03-08 13:12:12 +0000

    kommentar

---
+++ push.sh
@@ -0,0 +1,18 @@
+
+saveToGithub() {
+	echo "Ge en kommentar till dina ändringar!";
+	read kommentar;
+        git commit -m kommentar;
+        git push;
+}
+
+echo "Försöker spara alla filer."
+git add -A;
+git status;
+read -p "Vad statusen bra?(j/n)" jn;
+
+case "$jn" in
+   [Jj]* )  saveToGithub;;
+   [Nn]* )  exit;;
+       * )  echo "Var snäll och svara ja eller nej.";;
+esac
