61f6d5beb333cffb8a7622c424a950a62332f6ca
unknown <tskrm@D1099-W8.skolan.hassleholm.se>
2013-04-29 10:06:57 +0200

    kommentar

--- Demo/elever.php
+++ Demo/elever.php
@@ -10,7 +10,7 @@ include '../Demo/connect.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
 		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">    
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="se" lang="se">    
     <head>
         <meta http-equiv="Content-Type" content="text/html charset=utf-8" />
         <link rel="stylesheet" title="magnum" type="text/css" href="../CSS/magnum.css" />
